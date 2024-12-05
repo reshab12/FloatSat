83b848b8cd41b9c533de370427ec1d50420ba25a
j02g <jakob.geissler@outlook.de>
2024-12-05 13:10:35 +0100

    command ids for satellite modes

---
+++ include/commands.hpp
@@ -0,0 +1,10 @@
+#include "rodos.h"
+
+//mission modes
+const uint8_t command_id_mission_mode =             0x00;
+
+//controll modes
+const uint8_t command_id_control_mode =             0x01;
+
+//pose estimation modes
+const uint8_t command_id_pose_estimation_mode =     0x02;
