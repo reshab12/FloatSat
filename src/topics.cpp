4d97b788ae4ae63f95185db30491737f21748af0
j02g <jakob.geissler@outlook.de>
2024-12-05 13:07:50 +0100

    added commands for satellite modes

--- src/topics.cpp
+++ src/topics.cpp
@@ -9,3 +9,6 @@ Topic<position_data> topic_position_data(topic_id_position_data,"topic_position_
 Topic<control_value> topic_control_value(topic_id_control_value,"topic_control_value");
 Topic<additional_sensor_data> topic_additional_sensor_data(topic_id_additional_sensor_data,"topic_additional_sensor_data");
 Topic<satellite_mode> topic_satellite_mode(topic_id_satellite_mode,"topic_satellite_mode");
+
+// subscriber that fills the CommBuffer cb_satellite_mode with values from topic topic_satellite_mode
+Subscriber sub_satellite_mode(topic_satellite_mode, cb_satellite_mode_receiver_thread);
