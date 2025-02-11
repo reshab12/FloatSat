96b163677ddae3bad91aa2e61bc9fe584a48e089
j02g <jakob.geissler@outlook.de>
2025-02-11 14:49:54 +0100

    new gui

--- src/python_gui_test.py
+++ src/python_gui_test.py
@@ -5,7 +5,7 @@ import random
 import matplotlib
 matplotlib.use('QtAgg')
 
-from PyQt6 import QtCore, QtWidgets
+from PyQt6 import QtCore, QtWidgets, QtGui
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -40,89 +40,75 @@ class PlotWindow(QtWidgets.QMainWindow):
         super().__init__(*args, **kwargs)
 
         self.number_of_plots = 3
-        self.datasize = 39
-        self.number_of_boxes = 24
-        self.canvasss = [MplCanvas(self, width=5, height=4, dpi=100) for i in range(self.number_of_plots)]
-        self.plot_refs = [[None for i in range(5)] for j in range(self.number_of_plots)]#5=Max number of lines in one plot
-        
-        self.dataNames = ["Time",                       #0
-                          "pose_estimation_mode",       #1
-                          "control_mode",               #2
-                          "mission_mode",               #3
-                          "wx","wy","wz",               #4
-                          "mx","my","mz",               #7
-                          "ax","ay","az",               #10
-                          "headingMagneto",             #13
-                          "headingGyro",                #14
-                          "heading",                    #15
-                          "speed",                      #16
-                          "motorSpeed",                 #17
-                          "omega_wheel",                #18
-                          "control value",              #19
-                          "requested_angle",            #20
-                          "requested_rot_speed",        #21
-                          "user_requested_angle",       #22
-                          "user_requested_rot_speed",   #23
-                          "error",                      #24
-                          "Ierror",                     #25
-                          "error_change",               #26
-                          "Last_error",                 #27
-                          "error",                      #28
-                          "Ierror",                     #29
-                          "error_change",               #30
-                          "Last_error",                 #31
-                          "motorCurrent",               #32
-                          "magTorquerCurrent",          #33
-                          "boardCurrent",               #34
-                          "batterieVoltage",            #35
-                          "boardVoltage",               #36
-                          "increments",                 #37
-                          "turnDirection",              #38
-                          "raspberry_attitude",         #39
-                          "",
-                          "",
-                          ""
+        self.datasize = 40
+        self.number_of_boxes = 27
+         
+        self.dataNames = [["Time",True],                       #0
+                          ["pose_estimation_mode",True],       #1
+                          ["control_mode",True],               #2
+                          ["mission_mode",True],               #3
+                          ["wx",True],["wy",True],["wz",True],               #4
+                          ["mx",True],["my",True],["mz",True],               #7
+                          ["ax",False],["ay",False],["az",False],               #10
+                          ["headingMagneto",False],             #13
+                          ["headingGyro",False],                #14
+                          ["heading",True],                    #15
+                          ["speed",True],                      #16
+                          ["motorSpeed",True],                 #17
+                          ["omega_wheel",False],                #18
+                          ["control value",False],              #19
+                          ["requested_angle",False],            #20
+                          ["requested_rot_speed",False],        #21
+                          ["user_requested_angle",True],       #22
+                          ["user_requested_rot_speed",True],   #23
+                          ["error_vel",True],                      #24
+                          ["Ierror_vel",True],                     #25
+                          ["error_change_vel",True],               #26
+                          ["Last_error_vel",False],                 #27
+                          ["error_mot",True],                      #28
+                          ["Ierror_mot",True],                     #29
+                          ["error_change_mot",True],               #30
+                          ["Last_error_mot",False],                 #31
+                          ["motorCurrent",True],               #32
+                          ["magTorquerCurrent",True],          #33
+                          ["boardCurrent",True],               #34
+                          ["batterieVoltage",True],            #35
+                          ["boardVoltage",True],               #36
+                          ["increments",False],                 #37
+                          ["turnDirection",False],              #38
+                          ["raspberry_attitude",True],         #39
+                          ["",False],
+                          ["",False],
+                          ["",False],
+                          ["",False],
+                          ["",False],
+                          ["",False],
+                          ["",False],
+                          ["",False],
+                          ["",False]
                           ]
 
         self.dataFormat = [
-                        #[1,3],
-                        [4,3],
-                        #[7,3],
-                        #[10,3],
-                        [13,4],
-                        [17,1],
-                        #[18,1],
-                        #[19,1],
-                        #[20,4],
-                        #[24,3],
-                        #[27,1],
-                        #[28,4],
-                        #[32,5],
-                        #[37,1],
-                        #[39,1],
-                        [-1,0]
+                        [1,3,False,"modes"],
+                        [4,3,True,"gyr"],
+                        [7,3,False,"mag"],
+                        [10,3,False,"acc"],
+                        [13,4,True,"heading"],
+                        [17,1,False,"motor speed"],
+                        [18,1,False,"omega_wheel"],
+                        [19,1,False,"control value"],
+                        [20,4,False,"requested values"],
+                        [24,4,False,"vel errors"],
+                        [28,4,True,"mot errors"],
+                        [32,5,False,"currents"],
+                        [37,1,False,"increments"],
+                        [39,1,False,"raspberry_attitude"]
                         ]
 
-        layout = QtWidgets.QVBoxLayout()
-        for i in range(self.number_of_plots):
-            layout.addWidget(self.canvasss[i])
 
-        layout2 = QtWidgets.QVBoxLayout()
+        self.createLayout()
 
-        self.text_box =  [QtWidgets.QPushButton(self.dataNames[j])for j in range(self.number_of_boxes)]
-        for i in range(self.number_of_boxes):
-            self.text_box[i].setCheckable(False)
-            self.text_box[i].setMaximumWidth(300)
-            self.text_box[i].setText(self.dataNames[i])
-            layout2.addWidget(self.text_box[i])
-
-        h_layout = QtWidgets.QHBoxLayout()
-        h_layout.addLayout(layout2)
-        h_layout.addLayout(layout)
-
-        widget = QtWidgets.QWidget()
-        widget.setLayout(h_layout)
-        self.setCentralWidget(widget)
+        self.createMenu()
 
         self.n_data = 100
         self.xdata = list(range(self.n_data))
@@ -132,6 +118,124 @@ class PlotWindow(QtWidgets.QMainWindow):
 
         self.counter1 = 0
         self.counter2 = 0
+
+    def createLayout(self):
+        self.layout1 = QtWidgets.QVBoxLayout()
+        self.canvasss = [MplCanvas(self, width=5, height=4, dpi=100) for i in range(self.number_of_plots)]
+        self.plot_refs = [[None for i in range(5)] for j in range(self.number_of_plots)]#5=Max number of lines in one plot
+       
+        for i in range(self.number_of_plots):
+            self.layout1.addWidget(self.canvasss[i])
+
+        self.layout2 = QtWidgets.QVBoxLayout()
+
+        self.layout3 = QtWidgets.QVBoxLayout()
+
+        self.text_box =  [QtWidgets.QPushButton("")for j in range(self.number_of_boxes)]
+        for i in range(self.number_of_boxes):
+            self.text_box[i].setCheckable(False)
+            self.text_box[i].setMaximumWidth(250)
+            self.text_box[i].setText("")
+            self.layout2.addWidget(self.text_box[i])
+        
+        self.data_box =  [QtWidgets.QPushButton("")for j in range(self.number_of_boxes)]
+        for i in range(self.number_of_boxes):
+            self.data_box[i].setCheckable(False)
+            self.data_box[i].setMaximumWidth(100)
+            self.data_box[i].setText("")
+            self.layout3.addWidget(self.data_box[i])
+
+        self.h_layout = QtWidgets.QHBoxLayout()
+        self.h_layout.addLayout(self.layout2)
+        self.h_layout.addLayout(self.layout3)
+        self.h_layout.addLayout(self.layout1)
+
+        widget = QtWidgets.QWidget()
+        widget.setLayout(self.h_layout)
+        self.setCentralWidget(widget)
+
+    def removeLayouts(self):
+        for i in range(self.number_of_boxes):
+            self.layout2.removeWidget(self.text_box[i])
+            self.layout3.removeWidget(self.data_box[i])
+        self.clearPlotRefs()
+        for i in range(self.number_of_plots):
+            self.layout1.removeWidget(self.canvasss[i])
+        self.h_layout.removeItem(self.layout2)
+        self.h_layout.removeItem(self.layout3)
+        self.h_layout.removeItem(self.layout1)
+        self.takeCentralWidget()
+
+    def createMenu(self):
+        menu = self.menuBar()
+
+        file_menu = menu.addMenu("&View")
+
+        self.menuButtons = [QtGui.QAction(self.dataNames[i][0], self) for i in range(self.datasize)]
+        for i in range(self.datasize):
+            self.menuButtons[i].setCheckable(True)
+            if self.dataNames[i][1] == True:
+                self.menuButtons[i].setChecked(True)
+            self.menuButtons[i].triggered.connect(self.menuButtonsClicked)
+
+        for i in range(self.datasize):  
+            file_menu.addAction(self.menuButtons[i])
+
+        file_menu2 = menu.addMenu("&Plots")
+
+        self.menuPlots = []
+        j=0
+        for i in self.dataFormat:
+            self.menuPlots.append(QtGui.QAction(i[3], self))
+            self.menuPlots[j].setCheckable(True)
+            if i[2] == True:
+                self.menuPlots[j].setChecked(True)
+            self.menuPlots[j].triggered.connect(self.menuButtonsClicked)
+            file_menu2.addAction(self.menuPlots[j])
+            j += 1
+
+    def menuButtonsClicked(self):
+        self.removeLayouts()
+        #get new number of boxes:
+        self.number_of_boxes = 0
+        for i in range(self.datasize):
+            if self.menuButtons[i].isChecked():
+                self.dataNames[i][1] = True
+                self.number_of_boxes += 1
+            else: 
+                self.dataNames[i][1] = False
+        #print(self.number_of_boxes)
+
+        #get new number of plots:
+        j=0
+        self.number_of_plots = 0
+        for i in self.menuPlots:
+            if i.isChecked():
+                self.dataFormat[j][2] = True
+                self.number_of_plots += 1
+            else:
+                self.dataFormat[j][2] = False
+            j += 1
+
+        self.createLayout()
+        j=0
+        for i in range(self.number_of_boxes):
+            while((self.dataNames[j][1])==False):
+                j=j+1
+            self.text_box[i].setText(self.dataNames[j][0]+": ")
+            self.data_box[i].setText("{:.2f}".format(data[j]))
+            j=j+1
+    
+    def clearPlotRefs(self):
+        self.counter2 = 0
+        k=0
+        for i in self.dataFormat:
+            if i[2]:
+                for j in range(i[1]):
+                    self.plot_refs[k][j].axes.clear()
+                    self.plot_refs[k][j] = None
+                k += 1
+
 
     def update_plot(self,data):
         # Drop off the first y element, append a new one.
@@ -147,12 +251,12 @@ class PlotWindow(QtWidgets.QMainWindow):
             #textbox:
             j=0
             for i in range(self.number_of_boxes):
-                if(j==4):
-                    j=19
-                self.text_box[i].setText(self.dataNames[j]+": "+"{:.2f}".format(data[j]))
+                while((self.dataNames[j][1])==False):
+                    j=j+1
+                self.text_box[i].setText(self.dataNames[j][0]+": ")
+                self.data_box[i].setText("{:.2f}".format(data[j]))
                 j=j+1
        
-            #scaling factors for plots not used (autoscale)
             scaling = [5,20,5,4,200,10,10,10,10,10,10]
 
             data_line_count = 1
@@ -160,51 +264,54 @@ class PlotWindow(QtWidgets.QMainWindow):
             #redraw
             if self.counter2 > 39: 
                 self.counter2 = 0
+                self.clearPlotRefs()
+
+            if self.number_of_plots > 0:   
+                if self.plot_refs[0][0] == None:
+                    #print("=None")
+                    k=0
+                    for i in range(self.number_of_plots):
+                        while self.dataFormat[k][2] == False:
+                                k += 1
+                        data_line_count= self.dataFormat[k][0]
+                        for j in range(self.dataFormat[k][1]):
+                            match j:
+                                case 0:
+                                    temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'r',label=self.dataNames[data_line_count][0])
+                                case 1:
+                                    temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'g',label=self.dataNames[data_line_count][0])
+                                case 2:
+                                    temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'b',label=self.dataNames[data_line_count][0])
+                                case 3:
+                                    temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'c',label=self.dataNames[data_line_count][0])
+                                case 4:
+                                    temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'm',label=self.dataNames[data_line_count][0])
+                                    
+
+                            self.plot_refs[i][j] = temp_plot_refs[0] 
+                            
+                            self.plot_refs[i][j].axes.autoscale()
+                            
+                            data_line_count = data_line_count + 1
+                        self.plot_refs[i][0].axes.legend(loc="upper left")
+                        k += 1
+                #update data
+                else:
+                    #print("!=None")
+                    k=0
+                    for i in range(self.number_of_plots):
+                        while self.dataFormat[k][2] == False:
+                                k += 1
+                        data_line_count= self.dataFormat[k][0]
+                        for j in range(self.dataFormat[k][1]):
+                            self.plot_refs[i][j].set_ydata(self.ydata[data_line_count])
+
+                            data_line_count = data_line_count + 1
+                        k += 1
+
+                #update plot
                 for i in range(self.number_of_plots):
-                    for j in range(self.dataFormat[i][1]):
-                        self.plot_refs[i][j].axes.clear()
-                        self.plot_refs[i][j] = None
-                
-            if self.plot_refs[0][0] == None:
-                #print("=None")
-                for i in range(self.number_of_plots):
-                    if self.dataFormat[i][0] >= 0:
-                        data_line_count= self.dataFormat[i][0]
-                    for j in range(self.dataFormat[i][1]):
-                        match j:
-                            case 0:
-                                temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'r',label=self.dataNames[data_line_count])
-                            case 1:
-                                temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'g',label=self.dataNames[data_line_count])
-                            case 2:
-                                temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'b',label=self.dataNames[data_line_count])
-                            case 3:
-                                temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'c',label=self.dataNames[data_line_count])
-                            case 4:
-                                temp_plot_refs = self.canvasss[i].axes.plot(self.xdata, self.ydata[data_line_count], 'm',label=self.dataNames[data_line_count])
-                                
-
-                        self.plot_refs[i][j] = temp_plot_refs[0] 
-                        
-                        self.plot_refs[i][j].axes.autoscale()
-                        #self.plot_refs[i][j].axes.set_ylim(-scaling[i], scaling[i]) 
-
-                        data_line_count = data_line_count + 1
-                    self.plot_refs[i][0].axes.legend(loc="upper left")
-            #update data
-            else:
-                #print("!=None")
-                for i in range(self.number_of_plots):
-                    if self.dataFormat[i][0] >= 0:
-                        data_line_count= self.dataFormat[i][0]
-                    for j in range(self.dataFormat[i][1]):
-                        self.plot_refs[i][j].set_ydata(self.ydata[data_line_count])
-
-                        data_line_count = data_line_count + 1
-
-            #update plot
-            for i in range(self.number_of_plots):
-                self.canvasss[i].draw()
+                    self.canvasss[i].draw()
       
       
             
